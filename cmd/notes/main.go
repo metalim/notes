a7f7112fdccaa406fd134e266e250c595311a428
Maksim Litvinov <metalim@gmail.com>
2022-04-25 01:30:29 +0300

    add websocket handler draft

--- cmd/notes/main.go
+++ cmd/notes/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"notes/embedded"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
 
 const (
@@ -30,7 +30,40 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world!")
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer ws.Close()
+
+	err = ws.WriteMessage(websocket.TextMessage, []byte("Hello, Client!"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for {
+		mt, message, err := ws.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Printf("%s sent: %s", ws.RemoteAddr(), message)
+		err = ws.WriteMessage(mt, message)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
 
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/cortesi/modd v0.0.0-20211215124449-6083f9d1c171
 	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/websocket v1.5.0
 )
 
 require (
